50e8a18e837edf0ad95cc66a19e8c64bef8a20e5
Karl Omodt <90644248+komodt01@users.noreply.github.com>
2026-09-13 13:16:03 -0600

    Add deletion protection to GKE cluster configuration

--- terraform/gcp/main.tf
+++ terraform/gcp/main.tf
@@ -61,6 +61,7 @@ resource "google_container_cluster" "primary" {
 
   remove_default_node_pool = true
   initial_node_count       = 1
+  deletion_protection      = false
 
   workload_identity_config {
     workload_pool = "${var.project_id}.svc.id.goog"
@@ -79,7 +80,7 @@ resource "google_container_node_pool" "primary_nodes" {
   cluster  = google_container_cluster.primary.name
   location = var.zone
 
-  initial_node_count = 2
+  node_count = 2
 
   node_config {
     machine_type = "e2-medium"
