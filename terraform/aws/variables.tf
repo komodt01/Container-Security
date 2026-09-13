233fdbf0b205df5e34312c72aa766be9e5c40a8c
Karl Omodt <90644248+komodt01@users.noreply.github.com>
2026-09-13 13:12:59 -0600

    Add ACM certificate ARN variable
    Added a variable for ACM certificate ARN.

--- terraform/aws/variables.tf
+++ terraform/aws/variables.tf
@@ -3,3 +3,8 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "acm_certificate_arn" {
+  description = "ARN of the ACM certificate used by the HTTPS listener"
+  type        = string
+}
