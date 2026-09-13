952eb04b47f0a85c785284a97ecdf606a626a3e5
Karl Omodt <90644248+komodt01@users.noreply.github.com>
2026-09-13 13:16:31 -0600

    Replace artifact_registry_url output with new format

--- terraform/gcp/outputs.tf
+++ terraform/gcp/outputs.tf
@@ -2,6 +2,7 @@ output "cluster_name" {
   value = google_container_cluster.primary.name
 }
 
-output "artifact_registry_url" {
-  value = google_artifact_registry_repository.repo.id
+output "artifact_registry_repository" {
+  description = "Artifact Registry Docker repository path"
+  value       = "${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.repo.repository_id}"
 }
