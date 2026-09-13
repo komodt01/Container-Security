d126d6bd1fc27fa24c2d3c88dfb7c244537ab884
Karl Omodt <90644248+komodt01@users.noreply.github.com>
2026-09-13 12:57:04 -0600

    Add Google Cloud variables to variables.tf
    Added variables for Google Cloud project, region, and zone.

--- terraform/azure/variables.tf
+++ terraform/azure/variables.tf
@@ -1,5 +1,16 @@
-variable "location" {
-  description = "Azure region"
+variable "project_id" {
+  description = "Google Cloud project ID"
   type        = string
-  default     = "East US"
+}
+
+variable "region" {
+  description = "Google Cloud region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "Google Cloud zone"
+  type        = string
+  default     = "us-central1-a"
 }
